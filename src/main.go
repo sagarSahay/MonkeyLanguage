a2903b7cd92585b85e9c73d030c4bf50aae7aa59
sagarSahay <sagar.sahay@gmail.com>
2020-11-15 21:00:12 +0000

    Lexer implementation plus REPL implementation

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"buildInterpreter/src/repl"
+	"fmt"
+	"os"
+	user2 "os/user"
+)
+
+func main(){
+	user, err := user2.Current()
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey Programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
