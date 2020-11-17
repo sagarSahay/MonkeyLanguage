75d43f861484f84e27cfaec3eb4a7efb2e22a261
sagarSahay <sagar.sahay@gmail.com>
2020-11-17 06:54:16 +0000

    Implementing AST and basic outline for parser

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -41,5 +41,13 @@ func (ls *LetStatement) TokenLiteral() string  {
 }
 
 type Identifier struct {
-	
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()  {
+}
+
+func (i *Identifier) TokenLiteral() string{
+	return i.Token.Literal
 }
